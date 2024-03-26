d657e50d6657a4be5e78f68205a23ccd7f74ca27
Suikami Fubuchi <145380937+Suikami-Fubichi@users.noreply.github.com>
2024-03-26 14:43:31 -0700

    Fixed Infinity Jumping
    Goober will now only jump when grounded / bottom hitbox is colliding

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public Collision2D groundCol;
+    public bool grounded;
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void OnCollisionEnter2D(Collision2D groundCol)
+    {
+        grounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
+
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
-
+        
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             rb.velocity = new Vector2(0, 14f);
         }
+
     }
+
 }
