2d6ca2dc7d25620b02b2df034b2a1b52302111fc
Suikami Fubuchi <145380937+Suikami-Fubichi@users.noreply.github.com>
2024-04-03 17:20:51 -0700

    Made Character flip based on direction, changed box collision to capsule collision

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     public Collision2D groundCol;
+    private SpriteRenderer playerSprite;
     public bool grounded;
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>(); 
     }
 
     void OnCollisionEnter2D(Collision2D groundCol)
@@ -29,7 +32,14 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxisRaw("Horizontal");
         
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
-
+        if(dirX > 0)
+        {
+            playerSprite.flipX = false;
+        }
+        else if(dirX < 0)
+        {
+            playerSprite.flipX = true;
+        }
         if (Input.GetButtonDown("Jump") && grounded)
         {
             rb.velocity = new Vector2(0, 14f);
